9257b7eff0d1dafd00486dc4fa85a9f2592580db
g20271 <g20271@kagawa.kosen-ac.jp>
2023-07-07 02:02:52 +0900

    first commit

---
+++ Assets/Scripts/OscSender.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OscCore;
+
+public class OscSender : MonoBehaviour
+{
+    [SerializeField] string IpAddress = "10.2.0.247";
+    OscClient client;
+
+    public void OscStart()
+    {
+        client = new OscClient(IpAddress, 9000);
+    }
+
+    void Update()
+    {
+        if (!(client is null))
+        {
+            var rotRH = Input.gyro.attitude;
+            var rot = new Quaternion(-rotRH.x, -rotRH.z, -rotRH.y, rotRH.w) * Quaternion.Euler(90f, 0f, 0f);
+
+            client.Send("/sensor/w", rot.w);
+            client.Send("/sensor/x", rot.x);
+            client.Send("/sensor/y", rot.y);
+            client.Send("/sensor/z", rot.z);
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("Send");
+                client.Send("/hoge", 1.0f);
+            }
+        }
+    }
+
+    public void ChangedIpAddress(string InputIp)
+    {
+        IpAddress = InputIp;
+    }
+}
---
+++ Assets/Scripts/RotationInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotRH = Input.gyro.attitude;
+        var rot = new Quaternion(-rotRH.x, -rotRH.z, -rotRH.y, rotRH.w) * Quaternion.Euler(90f, 0f, 0f);
+        Debug.Log(rotRH);
+        Debug.Log(rot);
+
+        transform.localRotation = rot;
+        
+        //https://qiita.com/fuqunaga/items/b1a3e38af71f062f0781
+    }
+}
